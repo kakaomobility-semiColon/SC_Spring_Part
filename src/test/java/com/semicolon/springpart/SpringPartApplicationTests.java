ac0f6d860a8744b72a11a1a868c1dcdc10d51eb7
Jang Muyeong <jmy8876@gmail.com>
2024-04-15 11:16:32 +0900

    base

---
+++ src/test/java/com/semicolon/springpart/SpringPartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.springpart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
