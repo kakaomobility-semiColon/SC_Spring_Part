0c54cbeef248e7f50ae7cbfc17e33415bf86b291
Jang Muyeong <jmy8876@gmail.com>
2024-04-15 11:16:32 +0900

    base

---
+++ src/main/java/com/semicolon/springpart/SpringPartApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.springpart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPartApplication.class, args);
+	}
+
+}
