efb3df30708a94bd292f2a09c03d9ad25966e69a
Jang Muyeong <jmy8876@gmail.com>
2024-05-15 23:58:16 +0900

    # 캐시 구현 완료 (로컬 캐시 만료 시간 구현 위해 카페인 탑재/ 실행 유무 아직 확인 못함)

--- src/main/java/com/semicolon/springpart/Service/ChargerService.java
+++ src/main/java/com/semicolon/springpart/Service/ChargerService.java
@@ -3,8 +3,7 @@ package com.semicolon.springpart.Service;
 import com.semicolon.springpart.Repository.ChargerRepository;
 import com.semicolon.springpart.entity.ChargerApiEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +13,7 @@ import java.util.Optional;
 public class ChargerService {
 
     private final ChargerRepository chargerRepository;
+
 
     @Autowired
     public ChargerService(ChargerRepository chargerRepository) {
@@ -27,9 +27,13 @@ public class ChargerService {
         //이름이나 주소로 충전기 조회
         return chargerRepository.searchChargersByNameOrAddress(keyword);
     }
+
+    // 이 메서드에 캐시 적용, key를 lat, lng로 지정
+    @Cacheable(value = "searchChargersInAreaCache", key = "#swLat + ',' + #swLng + ',' + #neLat + ',' + #neLng")
     public List<ChargerApiEntity> searchChargersInArea(float swLat, float swLng, float neLat, float neLng) {
         // 북동쪽, 남서쪽 좌표를 기준으로 주변의 충전기 조회
-        return chargerRepository.findByLocationWithin(swLat, swLng, neLat, neLng);
+        List<ChargerApiEntity> chargers = chargerRepository.findByLocationWithin(swLat, swLng, neLat, neLng);
+        return chargers;
     }
 
         // id 기준 상세 정보 조회
--- src/main/java/com/semicolon/springpart/SpringPartApplication.java
+++ src/main/java/com/semicolon/springpart/SpringPartApplication.java
@@ -2,8 +2,12 @@ package com.semicolon.springpart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+
 
 @SpringBootApplication
+@EnableCaching
 public class SpringPartApplication {
 
 	public static void main(String[] args) {
